0e0b89f5961871806e3cadee587465478a364ced
L-Super <120311070@qq.com>
2023-04-07 09:51:14 +0800

    vault backup: on 2023-04-07 09:51:14 by Listening-Work

--- code/Strategy.cpp
+++ code/Strategy.cpp
@@ -1,3 +1,107 @@
 //
 // Created by Listening on 2023/4/7.
-//
+// 策略模式
+#include <iostream>
+#include <string>
+#include <memory>
+#include <string_view>
+/**
+ * Strategy接口声明某些算法的所有受支持版本通用的操作。
+ *
+ * Context使用此接口调用具体策略定义的算法。
+ */
+class Strategy
+{
+public:
+    virtual ~Strategy() = default;
+    virtual std::string doAlgorithm(std::string_view data) const = 0;
+};
+
+/**
+ * Context 定义客户端感兴趣的接口
+ */
+class Context
+{
+    /**
+     * @var Strategy Context 维护对其中一个策略对象的引用。上下文不知道策略的具体类别。它应该通过策略界面与所有策略一起使用。
+     */
+private:
+    std::unique_ptr<Strategy> strategy_;
+    /**
+     * Usually, the Context accepts a strategy through the constructor, but also
+     * provides a setter to change it at runtime.
+     */
+public:
+    explicit Context(std::unique_ptr<Strategy> &&strategy = {}) : strategy_(std::move(strategy))
+    {
+    }
+    /**
+     * Usually, the Context allows replacing a Strategy object at runtime.
+     */
+    void set_strategy(std::unique_ptr<Strategy> &&strategy)
+    {
+        strategy_ = std::move(strategy);
+    }
+    /**
+     * The Context delegates some work to the Strategy object instead of
+     * implementing +multiple versions of the algorithm on its own.
+     */
+    void doSomeBusinessLogic() const
+    {
+        if (strategy_) {
+            std::cout << "Context: Sorting data using the strategy (not sure how it'll do it)\n";
+            std::string result = strategy_->doAlgorithm("aecbd");
+            std::cout << result << "\n";
+        } else {
+            std::cout << "Context: Strategy isn't set\n";
+        }
+    }
+};
+
+/**
+ * Concrete Strategies implement the algorithm while following the base Strategy
+ * interface. The interface makes them interchangeable in the Context.
+ */
+class ConcreteStrategyA : public Strategy
+{
+public:
+    std::string doAlgorithm(std::string_view data) const override
+    {
+        std::string result(data);
+        std::sort(std::begin(result), std::end(result));
+
+        return result;
+    }
+};
+class ConcreteStrategyB : public Strategy
+{
+    std::string doAlgorithm(std::string_view data) const override
+    {
+        std::string result(data);
+        std::sort(std::begin(result), std::end(result), std::greater<>());
+
+        return result;
+    }
+};
+/**
+ * The client code picks a concrete strategy and passes it to the context. The
+ * client should be aware of the differences between strategies in order to make
+ * the right choice.
+ */
+
+void clientCode()
+{
+    Context context(std::make_unique<ConcreteStrategyA>());
+    std::cout << "Client: Strategy is set to normal sorting.\n";
+    context.doSomeBusinessLogic();
+    std::cout << "\n";
+    std::cout << "Client: Strategy is set to reverse sorting.\n";
+    context.set_strategy(std::make_unique<ConcreteStrategyB>());
+    context.doSomeBusinessLogic();
+}
+
+int main()
+{
+    clientCode();
+    return 0;
+}
